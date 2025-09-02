4508b785331a0a349d6bc10e2aa2dca6a3fc789a
Nathanael Smith <naSm1th@users.noreply.github.com>
2025-09-02 00:17:33 -0500

    Make CI use nix + flakes

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "Example flake environment for build buildroot projects";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ ];
+      systems = [ "x86_64-linux" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devShells.default = (pkgs.buildFHSEnv {
+          name = "buildroot";
+          targetPkgs = pkgs: (with pkgs;
+            [
+              (lib.hiPrio gcc)
+              bashInteractive
+              bc
+              binutils
+              bzip2
+              ccache # optional, speeds up consecutive builds
+              cmake
+              cpio
+              diffutils
+              expat # not mentioned in buildroot deps; dep of host-libxml-parser-perl
+              expect # not mentioned in buildroot deps
+              file
+              findutils
+              flock
+              gcc
+              glib # not mentioned; not sure if necessary
+              glibc # transitively mentioned: debian build-essential
+              gnumake
+              gnused
+              gnutar
+              gzip
+              libxcrypt # not mentioned in buildroot deps; required for host-mkpasswd
+              ncurses.dev # optional
+              patch
+              perl
+              pkg-config # not mentioned, unsure if necessary
+              rsync
+              swig
+              unzip
+              wget
+              which
+            ] ++ pkgs.linux.nativeBuildInputs);
+          shellHook = ''
+            # The host-uboot-tools package uses objcopy from the shells OBJCOPY var
+            # Since the var is set to OBJCOPY=objcopy the buildroot provided
+            # CROSS_COMPILE path is ignored hence the script is using the wrong objcopy
+            unset $OBJCOPY
+          '';
+        }).env;
+      };
+      flake = {
+      };
+    };
+}
