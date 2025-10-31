4a8493ccfa1a8df542fefc24e1bcc10f3e855845
Nathanael Smith <naSm1th@users.noreply.github.com>
2025-10-31 00:34:51 -0500

    Add amtrak-tracker-leds-tester package (#7)

---
+++ package/amtrak-tracker-led-tester/amtrak-tracker-led-tester.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# amtrak-tracker-led-tester
+#
+################################################################################
+
+AMTRAK_TRACKER_LED_TESTER_VERSION = 7c2333c2ea29b7aa965013c9f577388599005d4e
+AMTRAK_TRACKER_LED_TESTER_SOURCE = amtrak_tracker_led_tester-$(AMTRAK_TRACKER_LED_TESTER_VERSION).tar.gz
+AMTRAK_TRACKER_LED_TESTER_SITE = https://www.github.com/naSm1th/amtrak-tracker-led-tester.git
+AMTRAK_TRACKER_LED_TESTER_SITE_METHOD = git
+AMTRAK_TRACKER_LED_TESTER_INSTALL_TARGET = YES
+
+$(eval $(cargo-package))
